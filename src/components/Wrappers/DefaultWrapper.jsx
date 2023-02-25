cf3f67e343ed222ad13a0196c516f4780339db61
vetrovegor <vetroveg.72@mail.ru>
2023-02-26 00:09:39 +0500

    update

--- src/components/Wrappers/DefaultWrapper.jsx
+++ src/components/Wrappers/DefaultWrapper.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Outlet} from 'react-router-dom';
 
 import Header from '../Headers';
+import Footer from '../Footer';
 
 const DefaultWrapper = () => {
     return(
@@ -9,6 +10,8 @@ const DefaultWrapper = () => {
             <Header />
 
             <Outlet />
+
+            <Footer />
         </>
     )
 }
